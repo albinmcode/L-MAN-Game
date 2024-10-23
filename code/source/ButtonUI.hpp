161e964dee03fe95d3a750c9215292638f4229af
AXEL DE JESUS ROJAS RETANA <axel.rojasretana@ucr.ac.cr>
2024-10-23 15:54:47 +0000

    Replace ButtonUI.hpp

--- code/source/ButtonUI.hpp
+++ code/source/ButtonUI.hpp
@@ -8,10 +8,11 @@ using namespace std;
 
 class Button {
 public:
-    Button(const Vector2f& position, const string& textureFile);
-
+    Button(const Vector2f& position, const string& textureFile, const Vector2f& size = Vector2f(100, 50));
+    
     void draw(sf::RenderWindow& window);
     bool isMouseOver(const sf::RenderWindow& window) const;
+    void setSize(const Vector2f& size);  // Método para ajustar el tamaño
 
 private:
     RectangleShape buttonShape;
