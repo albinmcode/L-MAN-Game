6c5ed496584638ffb2429a4c0323010de3fbd635
Albin Gabriel Monge Arias <gabriel12mx@gmail.com>
2024-11-05 11:31:50 -0600

    small check colision fix

--- asm/loadMap.asm
+++ asm/loadMap.asm
@@ -49,9 +49,9 @@ checkColision PROC
     mov EDX, [ESP+12] ; y coordinate of the entity position (Row)
     
     ; Simplify coordinates scale, 32 -> 1
-    sub ECX, 65 ; substract the origine displayment in x
+    ;sub ECX, 65 ; substract the origine displayment in x
     ;sar ECX, 5 ; division by 32: 2^5, assume result in CL
-    sub EDX, 127 ; substract the origin displayment in y
+    ;sub EDX, 127 ; substract the origin displayment in y
     ;sar EDX, 5 ; division by 32: 2^5, assume result in DL
     
     ; Obtain the index in the map according to the coordinates (actualRow*totalColumns + actualColumn)
