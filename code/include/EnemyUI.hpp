2076cc83ab234dd1644003b329c910e36c75a710
JOSE PABLO BRENES COTO <josepablo.brenes@ucr.ac.cr>
2024-11-11 21:42:29 -0600

    change of include enemy

--- code/include/EnemyUI.hpp
+++ code/include/EnemyUI.hpp
@@ -2,11 +2,19 @@
 #include <SFML/Graphics.hpp>
 
 #include "EntityUI.hpp"
+#include <random>
+extern "C" std::int32_t movInput(std::int32_t*, std::int8_t);
 
 using namespace sf;
 
 class EnemyUI : public EntityUI {
 public:
 	// Default constructor
-	EnemyUI(Vector2f spawnPoint, const std::string& textureFile);
+	EnemyUI(Vector2f spawnPoint, const std::string& textureFile, std::int32_t corx, std::int32_t cory);
+	void actionEnemy(RenderWindow& window);
+	
+private:
+	static int32_t getRandom(std::int32_t min, std::int32_t max);
+	std::int8_t key=2;
+	void changeDirection();
 };
