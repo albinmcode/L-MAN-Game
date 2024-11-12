120fe5a4b8888834a3359a5430a6a887f00f1d67
JOSE PABLO BRENES COTO <josepablo.brenes@ucr.ac.cr>
2024-11-11 22:01:42 -0600

    Changes of include enemy 2

--- code/include/EnemyUI.hpp
+++ code/include/EnemyUI.hpp
@@ -10,11 +10,11 @@ using namespace sf;
 class EnemyUI : public EntityUI {
 public:
 	// Default constructor
-	EnemyUI(Vector2f spawnPoint, const std::string& textureFile, std::int32_t corx, std::int32_t cory);
-	void actionEnemy(RenderWindow& window);
+	EnemyUI(Vector2f spawnPoint, const std::string& textureFile, std::int32_t corx, std::int32_t cory); //Initializes the resources and positions needed for the enemy
+	void actionEnemy(RenderWindow& window); // Perform the movement, with different directions
 	
 private:
-	static int32_t getRandom(std::int32_t min, std::int32_t max);
-	std::int8_t key=2;
-	void changeDirection();
+	static int32_t getRandom(std::int32_t min, std::int32_t max); //Generates a random number with the computer clock
+	std::int8_t key=2; //Save the enemy's new address
+	void changeDirection(); //Change the random number to its respective letter in ascii and then use movinput
 };
