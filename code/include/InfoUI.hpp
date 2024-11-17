d552f2f92a1941a5fba9e2fc33dd493d3dd6207f
Albin Gabriel Monge Arias <gabriel12mx@gmail.com>
2024-11-16 23:45:16 -0600

    Info manual and credits update

--- code/include/InfoUI.hpp
+++ code/include/InfoUI.hpp
@@ -15,9 +15,12 @@ public:
     void reset();  // Recarga las texturas
 
 private:
-    Texture backgroundTexture;
+    Texture manualTexture;
+    Texture creditsTexture;
     Sprite backgroundSprite;
     Button backButton;  // Botón para regresar al menú principal
+    Button manualButton;  // Botón para ver instrucciones
+    Button creditsButton;  // Botón para ver créditos
 };
 
 #endif // INFOUI_HPP
--- code/src/InfoUI.cpp
+++ code/src/InfoUI.cpp
@@ -1,17 +1,22 @@
 #include "InfoUI.hpp"
 #include <iostream>
 
-InfoPage::InfoPage() :
-    backButton(Vector2f(20, 10), "assets/img/menu_salir.png", Vector2f(128, 47))  // Tamaño reducido
+InfoPage::InfoPage() 
+: backButton(Vector2f(20, 10), "assets/img/menu_salir.png", Vector2f(96, 48))
+, manualButton(Vector2f(128, 32), "assets/img/info_manual_click.png", Vector2f(288, 96))
+, creditsButton(Vector2f(352, 32), "assets/img/info_creditos_click.png", Vector2f(384, 96))
 {
     reset();
 }
 
 void InfoPage::reset() {
-    if (!backgroundTexture.loadFromFile("assets/img/info.png")) {
+    if (!manualTexture.loadFromFile("assets/img/info_manual.png")) {
         std::cerr << "Error al cargar la textura del fondo de información." << std::endl;
     }
-    backgroundSprite.setTexture(backgroundTexture);
+    if (!creditsTexture.loadFromFile("assets/img/info_creditos.png")) {
+        std::cerr << "Error al cargar la textura del fondo de información." << std::endl;
+    }
+    backgroundSprite.setTexture(manualTexture);
 
     // Escalar el fondo a 455x256
     // float scaleX = 910.0f / backgroundTexture.getSize().x;
@@ -24,10 +29,18 @@ void InfoPage::handleEvent(RenderWindow& window, Event& event, bool& backToMenu)
         if (backButton.isMouseOver(window)) {
             backToMenu = true;  // Volver al menú principal
         }
+        else if (manualButton.isMouseOver(window)) {
+            this->backgroundSprite.setTexture(manualTexture);
+        }
+        else if (creditsButton.isMouseOver(window)) {
+            this->backgroundSprite.setTexture(creditsTexture);
+        }
     }
 }
 
 void InfoPage::draw(RenderWindow& window) {
     window.draw(backgroundSprite);
     backButton.draw(window);
+    manualButton.draw(window);
+    creditsButton.draw(window);
 }
