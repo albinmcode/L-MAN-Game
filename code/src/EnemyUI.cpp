c109ed59e3814a526f5493bb942b08da810f7cec
JOSE PABLO BRENES COTO <josepablo.brenes@ucr.ac.cr>
2024-12-03 22:06:25 -0600

    Complete enemy movement, complete

--- code/src/EnemyUI.cpp
+++ code/src/EnemyUI.cpp
@@ -8,7 +8,7 @@ Constructor initializes the enemy object at a given spawn point, sets the scaled
 and initial pixel position based on grid coordinates, sets the initial movement direction,
 and loads textures for the enemy. Sets the right texture as default for the sprite.
 */
-EnemyUI::EnemyUI(Vector2f spawnPoint, const std::string& textureFile, std::int32_t corx, std::int32_t cory, EntityUI& target)
+EnemyUI::EnemyUI(Vector2f spawnPoint, const std::string& textureFile,const std::string& vulnerableTextureFile, std::int32_t corx, std::int32_t cory, EntityUI& target)
 : EntityUI(spawnPoint) 
 , target(target) {
     this->scaledPosition = Vector2<int32_t>(corx, cory);
