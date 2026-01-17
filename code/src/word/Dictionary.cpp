d303c3e5c700ffd9cf970f002deac24a9b7c80ed
Albin Monge Arias <albin.monge@ucr.ac.cr>
2026-01-17 08:09:55 -0600

    fix empty comparison on loadFromCSV

--- code/src/word/Dictionary.cpp
+++ code/src/word/Dictionary.cpp
@@ -21,7 +21,7 @@ bool Dictionary::loadFromCSV(std::string path) {
     dictIndex newIndex;
     delimiter_pos = line.find(',');
     // skip empty lines
-    if (delimiter_pos = std::string::npos) continue;
+    if (delimiter_pos == std::string::npos) continue;
     // separate index
     newIndex.word = line.substr(0, delimiter_pos);
     newIndex.translation = line.substr(delimiter_pos + 1);
